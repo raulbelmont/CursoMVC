d764556e49925f3c6cb4768577b8a752e457fb63
Raul Belmonte <raulbelomont@gmail.com>
2019-09-13 21:29:58 -0300

    Instalando PHPMAILER com o Composer

--- src/vendor/composer/autoload_static.php
+++ src/vendor/composer/autoload_static.php
@@ -6,9 +6,25 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit0bc4dee9efc16bdbfa46ab79a06f34f8
 {
+    public static $prefixLengthsPsr4 = array (
+        'P' => 
+        array (
+            'PHPMailer\\PHPMailer\\' => 20,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'PHPMailer\\PHPMailer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit0bc4dee9efc16bdbfa46ab79a06f34f8::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit0bc4dee9efc16bdbfa46ab79a06f34f8::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
